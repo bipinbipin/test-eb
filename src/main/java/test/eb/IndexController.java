e0410f7546f23d1b6909c3b101b6b0ab765cae26
bipin <bipinbipin@gmail.com>
2017-03-10 14:49:28 -0600

    Initial commit

---
+++ src/main/java/test/eb/IndexController.java
@@ -0,0 +1,21 @@
+package test.eb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by bipin on 3/10/2017.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ src/main/java/test/eb/TestEbApplication.java
@@ -0,0 +1,12 @@
+package test.eb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEbApplication.class, args);
+	}
+}
